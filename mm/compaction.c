8f54cdc629ef1e5dd604cb013505e4b24e074276
Tim Murray <timmurray@google.com>
2016-08-03 18:27:46 -0700

    BACKPORT: mm: fix pageblock heuristic
    The Android-tuned page block heuristic was accidentally reset in an AU
    drop. Fix the heuristic to avoid unnecessary unmovable pageblock
    migration over time.
    
    bug 30643938
    Bug: 63336523
    (cherry-picked from commit 3e19bcf7d08713daaaba888b4d13502e06e38e96)
    Change-Id: I59efcd3934f29982b1c9aeb7b0f18eb17e0934b3
    Signed-off-by: John Dias <joaodias@google.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1903,7 +1903,7 @@ static enum compact_result __compact_finished(struct compact_control *cc)
 		 * other migratetype buddy lists.
 		 */
 		if (find_suitable_fallback(area, order, migratetype,
-						true, &can_steal) != -1) {
+						true, cc->order, &can_steal) != -1) {
 
 			/* movable pages are OK in any pageblock */
 			if (migratetype == MIGRATE_MOVABLE)
