3fb7aaaef1065f782b8d73b56f32fb32741a420f
jimzrt <james.tophoven@gmail.com>
2018-02-08 12:55:48 +0100

    usb: Modify mass_storage gadget to work with configfs
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -3937,6 +3937,8 @@ static void fsg_free_inst(struct usb_function_instance *fi)
 	kfree(opts);
 }
 
+extern struct device *create_function_device(char *name);
+
 static struct usb_function_instance *fsg_alloc_inst(void)
 {
 	struct fsg_opts *opts;
@@ -3987,6 +3989,9 @@ static struct usb_function_instance *fsg_alloc_inst(void)
 	}
 #endif
 	configfs_add_default_group(&opts->lun0.group, &opts->func_inst.group);
+
+	//create dummy device
+	create_function_device("f_mass_storage");
 
 	return &opts->func_inst;
 
