8654941bed4516c5e3ebe416ffd5d83b73033d37
Minchan Kim <minchan@google.com>
2018-08-13 08:15:32 +0900

    mm: mm_event: add read io stat
    Read IO's latency as well as filemap fault could affect system
    performance so this patch keeps track it on.
    
    Mot-CRs-fixed: (CR)
    
    Bug: 80168800
    Change-Id: I761b7110339cf1e5ef24530ad32aedd784d00d07
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    Reviewed-on: https://gerrit.mot.com/1453725
    Tested-by: Jira Key
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- mm/filemap.c
+++ mm/filemap.c
@@ -2036,7 +2036,9 @@ static ssize_t generic_file_buffered_read(struct kiocb *iocb,
 		pgoff_t end_index;
 		loff_t isize;
 		unsigned long nr, ret;
+		ktime_t event_ts;
 
+		event_ts = 0;
 		cond_resched();
 find_page:
 		if (fatal_signal_pending(current)) {
@@ -2048,6 +2050,7 @@ find_page:
 		if (!page) {
 			if (iocb->ki_flags & IOCB_NOWAIT)
 				goto would_block;
+			mm_event_start(&event_ts);
 			page_cache_sync_readahead(mapping,
 					ra, filp,
 					index, last_index - index);
@@ -2094,6 +2097,8 @@ find_page:
 			unlock_page(page);
 		}
 page_ok:
+		if (event_ts != 0)
+			mm_event_end(MM_READ_IO, event_ts);
 		/*
 		 * i_size must be checked after we know the page is Uptodate.
 		 *
