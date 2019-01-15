45acc5faabe4a7cdfbd1e95360015c20cecf8a6a
Minchan Kim <minchan@google.com>
2019-01-15 13:54:07 +0900

    mm: mm_event supports vmstat
    Vmstat is significantly important to investigate MM problem.
    We have solved many problmes with it via asking users to get
    vmstat data periodically from the device, which manual way is
    painful once we release the device or on hard reproducible
    scenario.
    
    This patch adds periodic vmstat dump into mm_event. It works
    only if there are some events in compaction or reclaim. Thus,
    unless there is memory pressure, it doesn't gather any vmstat
    data. Defenderault interval between each dump is 1000ms.
    Admin can tweak it via
    
    	echo 2000 > /sys/kernel/debug/mm_event/vmstat_period_ms
    
    Mot-CRs-fixed: (CR)
    
    Bug: 80168800
    Change-Id: I4c0e7237d7764c4ea79da00952e5de34ccbe4187
    Signed-off-by: Minchan Kim <minchan@google.com>
    Reviewed-on: https://gerrit.mot.com/1453728
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- include/linux/mm_event.h
+++ include/linux/mm_event.h
@@ -24,6 +24,23 @@ struct mm_event_task {
 
 struct task_struct;
 
+struct mm_event_vmstat {
+	unsigned long free;
+	unsigned long file;
+	unsigned long anon;
+	unsigned long slab;
+	unsigned long ws_refault;
+	unsigned long ws_activate;
+	unsigned long mapped;
+	unsigned long pgin;
+	unsigned long pgout;
+	unsigned long swpin;
+	unsigned long swpout;
+	unsigned long reclaim_steal;
+	unsigned long reclaim_scan;
+	unsigned long compact_scan;
+};
+
 #ifdef CONFIG_MM_EVENT_STAT
 void mm_event_task_init(struct task_struct *tsk);
 void mm_event_start(ktime_t *time);
