e4ced517a01f01ad8dd4174e00bd3e6dab69031e
Diep Quynh <remilia.1505@gmail.com>
2019-04-15 11:49:52 +0700

    devfreq_boost: Adapt to Exynos 9610
    Exynos9610: Adapt devfreq_boost to 4.14 and Exynos 9610
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: John Vincent <git@tensevntysevn.cf>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -6,7 +6,7 @@
 #define pr_fmt(fmt) "devfreq_boost: " fmt
 
 #include <linux/devfreq_boost.h>
-#include <linux/msm_drm_notify.h>
+#include <linux/fb.h>
 #include <linux/input.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
@@ -30,7 +30,7 @@ struct boost_dev {
 
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
-	struct notifier_block msm_drm_notif;
+	struct notifier_block fb_notif;
 };
 
 static void devfreq_input_unboost(struct work_struct *work);
@@ -49,8 +49,8 @@ static void devfreq_max_unboost(struct work_struct *work);
 }
 
 static struct df_boost_drv df_boost_drv_g __read_mostly = {
-	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_MSM_CPUBW,
-		       CONFIG_DEVFREQ_MSM_CPUBW_BOOST_FREQ)
+	BOOST_DEV_INIT(df_boost_drv_g, DEVFREQ_EXYNOS_MIF,
+		       CONFIG_DEVFREQ_EXYNOS_MIF_BOOST_FREQ)
 };
 
 static void __devfreq_boost_kick(struct boost_dev *b)
@@ -177,23 +177,21 @@ static int devfreq_boost_thread(void *data)
 	return 0;
 }
 
-static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
+static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 			  void *data)
 {
-	struct df_boost_drv *d = container_of(nb, typeof(*d), msm_drm_notif);
-	int i;
-	struct msm_drm_notifier *evdata = data;
-	int *blank = evdata->data;
+	struct df_boost_drv *d = container_of(nb, typeof(*d), fb_notif);
+	int i, *blank = ((struct fb_event *)data)->data;
 
 	/* Parse framebuffer blank events as soon as they occur */
-	if (action != MSM_DRM_EARLY_EVENT_BLANK)
+	if (action != FB_EARLY_EVENT_BLANK)
 		return NOTIFY_OK;
 
 	/* Boost when the screen turns on and unboost when it turns off */
 	for (i = 0; i < DEVFREQ_MAX; i++) {
 		struct boost_dev *b = d->devices + i;
 
-		if (*blank == MSM_DRM_BLANK_UNBLANK) {
+		if (*blank == FB_BLANK_UNBLANK) {
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
@@ -315,9 +313,9 @@ static int __init devfreq_boost_init(void)
 		goto stop_kthreads;
 	}
 
-	d->msm_drm_notif.notifier_call = msm_drm_notifier_cb;
-	d->msm_drm_notif.priority = INT_MAX;
-	ret = msm_drm_register_client(&d->msm_drm_notif);
+	d->fb_notif.notifier_call = fb_notifier_cb;
+	d->fb_notif.priority = INT_MAX;
+	ret = fb_register_client(&d->fb_notif);
 	if (ret) {
 		pr_err("Failed to register fb notifier, err: %d\n", ret);
 		goto unregister_handler;
