4ffaa5d1b6f162d4a57d4b8eadb792c62a918c49
Sahitya Tummala <stummala@codeaurora.org>
2019-05-10 22:00:33 -0400

    ext4: fix use-after-free in dx_release()
    commit 08fc98a4d6424af66eb3ac4e2cedd2fc927ed436 upstream.
    
    The buffer_head (frames[0].bh) and it's corresping page can be
    potentially free'd once brelse() is done inside the for loop
    but before the for loop exits in dx_release(). It can be free'd
    in another context, when the page cache is flushed via
    drop_caches_sysctl_handler(). This results into below data abort
    when accessing info->indirect_levels in dx_release().
    
    Unable to handle kernel paging request at virtual address ffffffc17ac3e01e
    Call trace:
     dx_release+0x70/0x90
     ext4_htree_fill_tree+0x2d4/0x300
     ext4_readdir+0x244/0x6f8
     iterate_dir+0xbc/0x160
     SyS_getdents64+0x94/0x174
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Andreas Dilger <adilger@dilger.ca>
    Cc: stable@kernel.org
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: John Vincent <git@tensevntysevn.cf>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -870,11 +870,14 @@ static void dx_release(struct dx_frame *frames)
 {
 	struct dx_root_info *info;
 	int i;
+	unsigned int indirect_levels;
 
 	if (frames[0].bh == NULL)
 		return;
 
 	info = &((struct dx_root *)frames[0].bh->b_data)->info;
+	/* save local copy, "info" may be freed after brelse() */
+	indirect_levels = info->indirect_levels;
 	for (i = 1; i <= info->indirect_levels; i++) {
 		if (frames[i].bh == NULL)
 			break;
