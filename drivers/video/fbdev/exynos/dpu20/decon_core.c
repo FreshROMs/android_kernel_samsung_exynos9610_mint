2ee208f81ac00c3e81e7f58bf11d8d4af4f03370
Diep Quynh <remilia.1505@gmail.com>
2019-06-05 16:35:52 +0700

    exynos: decon: Boost MIF devfreq device when a new frame is ready
    The cpubw devfreq device delegates the speed of the DDR bus, so its
    speed is directly correlated with frame drops. Boost it when a new frame
    is ready to be rendered to the display to reduce overall frame drops.
    
    https://github.com/kerneltoast/android_kernel_google_wahoo/commit/874722fda8b28e55e595e12977fbb2de4f7f1042
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -54,6 +54,7 @@
 #include "./panels/lcd_ctrl.h"
 #include "../../../../dma-buf/sync_debug.h"
 #include "dpp.h"
+#include <linux/devfreq_boost.h>
 #if defined(CONFIG_EXYNOS_DISPLAYPORT)
 #include "displayport.h"
 #endif
@@ -2659,6 +2660,7 @@ static int decon_set_win_config(struct decon_device *decon,
 
 	num_of_window = decon_get_active_win_count(decon, win_data);
 	if (num_of_window) {
+	    devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 70);
 		win_data->retire_fence = decon_create_fence(decon, &sync_file);
 		if (win_data->retire_fence < 0)
 			goto err_prepare;
@@ -2684,6 +2686,7 @@ static int decon_set_win_config(struct decon_device *decon,
 			sizeof(struct decon_rect));
 
 	if (num_of_window) {
+		devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 70);
 		decon_create_release_fences(decon, win_data, sync_file);
 #if !defined(CONFIG_SUPPORT_LEGACY_FENCE)
 		regs->retire_fence = dma_fence_get(sync_file->fence);
