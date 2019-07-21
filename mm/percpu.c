dcd4b1dd8f13a2c6eae6178c51acd766fd05ef7b
Christophe JAILLET <christophe.jaillet@wanadoo.fr>
2019-07-21 11:56:33 +0200

    percpu: fix typo in pcpu_setup_first_chunk() comment
    s/perpcu/percpu/
    
    Signed-off-by: Christophe JAILLET <christophe.jaillet@wanadoo.fr>
    Signed-off-by: Dennis Zhou <dennis@kernel.org>
    [Dennis: updated title]
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- mm/percpu.c
+++ mm/percpu.c
@@ -2185,7 +2185,7 @@ static void pcpu_dump_alloc_info(const char *lvl,
  * @base_addr: mapped address
  *
  * Initialize the first percpu chunk which contains the kernel static
- * perpcu area.  This function is to be called from arch percpu area
+ * percpu area.  This function is to be called from arch percpu area
  * setup path.
  *
  * @ai contains all information necessary to initialize the first
