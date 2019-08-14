1f2da54b1407f4ccc5043982c1dc9b772cc37b57
corsicanu <corsicanu22@gmail.com>
2019-08-14 18:01:04 +0300

    net: allow knox to be disabled
    Signed-off-by: corsicanu <corsicanu22@gmail.com>
    
    # Conflicts:
    #	net/core/sock.c
    #	net/ipv4/udp.c
    #	net/netfilter/nf_conntrack_core.c
    
    # Notes:
    # Samsung "seriously" changed how they named these. ._.

--- net/netfilter/nf_conntrack_core.c
+++ net/netfilter/nf_conntrack_core.c
@@ -57,9 +57,12 @@
 #include <net/netns/hash.h>
 
 #include "nf_internals.h"
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+
+#ifdef CONFIG_KNOX_NCM
+// KNOX NPA - START
 #include <net/ncm.h>
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+// KNOX NPA - END
+#endif
 
 #define NF_CONNTRACK_VERSION	"0.5.0"
 
@@ -319,13 +322,16 @@ static void nf_ct_add_to_dying_list(struct nf_conn *ct)
 {
 	struct ct_pcpu *pcpu;
 
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
+	// KNOX NPA - START
 	/* Add 'del_timer(&ct->npa_timeout)' if struct nf_conn->timeout is of type struct timer_list; */
 	/* send dying conntrack entry to collect data */
 	if ( (check_ncm_flag()) && (ct != NULL) && (atomic_read(&ct->startFlow)) ) {
 		knox_collect_conntrack_data(ct, NCM_FLOW_TYPE_CLOSE, 10);
 	}
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+	// KNOX NPA - END
+#endif
+
 	/* add this conntrack to the (per cpu) dying list */
 	ct->cpu = smp_processor_id();
 	pcpu = per_cpu_ptr(nf_ct_net(ct)->ct.pcpu_lists, ct->cpu);
@@ -1052,15 +1058,17 @@ static void gc_worker(struct work_struct *work)
 				nf_ct_gc_expired(tmp);
 				expired_count++;
 				continue;
-			// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
+			// KNOX NPA - START	
 			} else if ( (tmp != NULL) && (check_ncm_flag()) && (check_intermediate_flag()) && (atomic_read(&tmp->startFlow)) && (atomic_read(&tmp->intermediateFlow)) ) {
 				s32 npa_timeout = tmp->npa_timeout - ((u32)(jiffies));
 				if (npa_timeout <= 0) {
 					tmp->npa_timeout = ((u32)(jiffies)) + (get_intermediate_timeout() * HZ);
 					knox_collect_conntrack_data(tmp, NCM_FLOW_TYPE_INTERMEDIATE, 20);
 				}
+			// KNOX NPA - END
+#endif
 			}
-			// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
 
 			if (nf_conntrack_max95 == 0 || gc_worker_skip_ct(tmp))
 				continue;
@@ -1115,11 +1123,13 @@ static void gc_worker(struct work_struct *work)
 	ratio = scanned ? expired_count * 100 / scanned : 0;
 	if (ratio > GC_EVICT_RATIO) {
 		gc_work->next_gc_run = min_interval;
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
+		// KNOX NPA - START
 		if ( (check_ncm_flag()) && (check_intermediate_flag()) ) {
 			gc_work->next_gc_run = 0;
 		}
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+		// KNOX NPA - END
+#endif
 	} else {
 		unsigned int max = GC_MAX_SCAN_JIFFIES / GC_MAX_BUCKETS_DIV;
 
@@ -1128,11 +1138,13 @@ static void gc_worker(struct work_struct *work)
 		gc_work->next_gc_run += min_interval;
 		if (gc_work->next_gc_run > max)
 			gc_work->next_gc_run = max;
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
+		// KNOX NPA - START
 		if ( (check_ncm_flag()) && (check_intermediate_flag()) ) {
 			gc_work->next_gc_run = 0;
 		}
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+		// KNOX NPA - END
+#endif
 	}
 
 	next_run = gc_work->next_gc_run;
@@ -1156,9 +1168,11 @@ __nf_conntrack_alloc(struct net *net,
 		     gfp_t gfp, u32 hash)
 {
 	struct nf_conn *ct;
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
+	// KNOX NPA - START
 	struct timespec open_timespec;
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+	// KNOX NPA - END
+#endif
 
 	/* We don't want any race condition at early drop stage */
 	atomic_inc(&net->ct.count);
@@ -1183,7 +1197,9 @@ __nf_conntrack_alloc(struct net *net,
 		goto out;
 
 	spin_lock_init(&ct->lock);
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+
+#ifdef CONFIG_KNOX_NCM
+	// KNOX NPA - START
 	/* initialize the conntrack structure members when memory is allocated */
 	if (ct != NULL) {
 		open_timespec = current_kernel_time();
@@ -1204,7 +1220,9 @@ __nf_conntrack_alloc(struct net *net,
 		ct->npa_timeout = 0;
 		atomic_set(&ct->intermediateFlow, 0);
 	}
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+	// KNOX NPA - END
+#endif
+
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].tuple = *orig;
 	ct->tuplehash[IP_CT_DIR_ORIGINAL].hnnode.pprev = NULL;
 	ct->tuplehash[IP_CT_DIR_REPLY].tuple = *repl;
