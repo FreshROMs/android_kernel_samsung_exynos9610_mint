f428d854724d9b2076bd9a24abd096bb9520e8ed
LuK1337 <priv.luk@gmail.com>
2019-08-18 00:54:54 +0200

    input: melfas_mss100: Use KEY_WAKEUP for DT2W/AOT
    Change-Id: I86b384a07c425d1274a435ee6dff1f68c26596c9
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Signed-off-by: santhosh6194 <santhosh6194@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/input/touchscreen/melfas/mss100/melfas_mss100_mod.c
+++ drivers/input/touchscreen/melfas/mss100/melfas_mss100_mod.c
@@ -589,9 +589,9 @@ int mms_custom_event_handler(struct mms_ts_info *info, u8 *rbuf, u8 size)
 				input_sync(info->input_dev);
 			} else if (gesture_id == MMS_GESTURE_ID_DOUBLETAP_TO_WAKEUP) {
 				input_info(true, &info->client->dev, "%s: AOT\n", __func__);
-				input_report_key(info->input_dev, KEY_HOMEPAGE, 1);
+				input_report_key(info->input_dev, KEY_WAKEUP, 1);
 				input_sync(info->input_dev);
-				input_report_key(info->input_dev, KEY_HOMEPAGE, 0);
+				input_report_key(info->input_dev, KEY_WAKEUP, 0);
 			}
 		} else if (gesture_type == MMS_GESTURE_CODE_SINGLE_TAP) {
 			info->scrub_id = SPONGE_EVENT_TYPE_SINGLE_TAP;
@@ -767,7 +767,7 @@ void mms_config_input(struct mms_ts_info *info)
 	set_bit(EV_KEY, input_dev->evbit);
 	set_bit(KEY_POWER, input_dev->keybit);
 #endif
-	set_bit(KEY_HOMEPAGE, input_dev->keybit);
+	set_bit(KEY_WAKEUP, input_dev->keybit);
 	set_bit(KEY_BLACK_UI_GESTURE, input_dev->keybit);
 	input_dbg(true, &info->client->dev, "%s [DONE]\n", __func__);
 }
