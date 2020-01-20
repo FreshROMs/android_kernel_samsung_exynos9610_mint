51702b1f3ca0132d1b1547abbca22b9e1983338c
Cho KyongHo <pullip.cho@samsung.com>
2020-01-20 19:45:31 +0900

    g2d: do not gate clock if no gate clock is defined
    The gate clock to G2D may not be defined in some environment if the
    clock is always enabled or is automatically enabled and gated behind
    the driver.
    
    Change-Id: Ia0ad0188bbd30c51c35d777d07ff9c93451bf832
    Signed-off-by: Cho KyongHo <pullip.cho@samsung.com>
    (cherry picked from commit 84a1b4b61e7f1ca8cef1b3bb17506e7e291afc55)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 165562344
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/exynos/g2d/g2d_drv.c
+++ drivers/gpu/exynos/g2d/g2d_drv.c
@@ -919,7 +919,10 @@ static int g2d_probe(struct platform_device *pdev)
 	dma_set_mask(&pdev->dev, DMA_BIT_MASK(36));
 
 	g2d_dev->clock = devm_clk_get(&pdev->dev, "gate");
-	if (IS_ERR(g2d_dev->clock)) {
+	if (PTR_ERR(g2d_dev->clock) == -ENOENT) {
+		perrdev(g2d_dev, "'gate' not found. Ignoring clock gating..\n");
+		g2d_dev->clock = NULL;
+	} else if (IS_ERR(g2d_dev->clock)) {
 		ret = PTR_ERR(g2d_dev->clock);
 		perrdev(g2d_dev, "Failed to get clock (%d)", ret);
 		return ret;
