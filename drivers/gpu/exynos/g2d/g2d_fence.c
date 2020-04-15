08b187a126e305953f7a43efbf6b24c494d1edab
THEBOSS <ehabhani59@gmail.com>
2020-04-15 04:09:11 +0300

    exynos/g2d: Remove naming mechanism on fences
    It lowers CPU time to be spent on the strlcpy and prevent 32 bytes of wastage for those naming fences... it is proven on several files that those naming fences are just there for debugging purposes only, yet.. it might cause suboptimal operations for others just because naming fences mechanism which at the end... it is only made for developmental purposes... not for users.
    
    Inspired and Adapted from https://github.com/kerneltoast/android_kernel_google_wahoo/commit/24627e6b0d7042f0c28b79a85f43f8d2f2f816d3
    
    As the original commit said... [Eliminate unnecessary operations on fence names.]
    
    Signed-off-by: repinger <evanf780@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/exynos/g2d/g2d_fence.c
+++ drivers/gpu/exynos/g2d/g2d_fence.c
@@ -18,31 +18,16 @@
 void g2d_fence_timeout_handler(unsigned long arg)
 {
 	struct g2d_task *task = (struct g2d_task *)arg;
-	struct g2d_device *g2d_dev = task->g2d_dev;
 	struct dma_fence *fence;
 	unsigned long flags;
-	char name[32];
 	int i;
 	s32 afbc = 0;
 
-	for (i = 0; i < task->num_source; i++) {
+	for (i = 0; i < task->num_source; i++)
 		fence = task->source[i].fence;
-		if (fence) {
-			strlcpy(name, fence->ops->get_driver_name(fence),
-			       sizeof(name));
-			perrfndev(g2d_dev, " SOURCE[%d]:  %s #%d (%s)",
-				  i, name, fence->seqno,
-				  dma_fence_is_signaled(fence)
-					? "signaled" : "active");
-		}
-	}
+
 
 	fence = task->target.fence;
-	if (fence) {
-		strlcpy(name, fence->ops->get_driver_name(fence), sizeof(name));
-		perrfn(" TARGET:     %s #%d (%s)", name, fence->seqno,
-		       dma_fence_is_signaled(fence) ? "signaled" : "active");
-	}
 
 	if (task->release_fence)
 		perrfn("   Pending g2d release fence: #%d",
