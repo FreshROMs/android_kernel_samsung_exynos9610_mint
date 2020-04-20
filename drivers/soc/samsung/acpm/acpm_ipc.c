0048e0a5244efd1d49a956a89f3f8e75cd6cd2c8
Diep Quynh <remilia.1505@gmail.com>
2020-04-20 22:16:47 +0700

    samsung: acpm: Disable logging by default
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/soc/samsung/acpm/acpm_ipc.c
+++ drivers/soc/samsung/acpm/acpm_ipc.c
@@ -31,7 +31,7 @@ static struct acpm_ipc_info *acpm_ipc;
 static struct workqueue_struct *debug_logging_wq;
 static struct workqueue_struct *update_log_wq;
 static struct acpm_debug_info *acpm_debug;
-static bool is_acpm_stop_log = false;
+static bool is_acpm_stop_log = true;
 static bool acpm_stop_log_req = false;
 volatile struct acpm_framework *acpm_initdata;
 void __iomem *acpm_srambase;
