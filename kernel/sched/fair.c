26bed6120a7673928e40c88d8f244516a0ea1e55
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-09 17:32:32 +0200

    kernel/sched: Support for Gentle_fair_sleepers
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3969,6 +3969,13 @@ static void check_spread(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 }
 
+static unsigned int Lgentle_fair_sleepers = 0;
+
+void relay_gfs(unsigned int gfs)
+{
+	Lgentle_fair_sleepers = gfs;
+}
+
 static void
 place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 {
@@ -3997,7 +4004,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 		 * Halve their sleep time's effect, to allow
 		 * for a gentler effect of sleepers:
 		 */
-		if (sched_feat(GENTLE_FAIR_SLEEPERS))
+		if (Lgentle_fair_sleepers)
 			thresh >>= 1;
 
 		vruntime -= thresh;
