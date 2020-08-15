4921cb788413c415259a31fac4ba1b6e2149a507
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-15 17:47:11 +0200

    blu_schedutil: fix DOWN_RATE_LIMIT_US from 400 to 4000
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -35,7 +35,7 @@ unsigned long boosted_cpu_util(int cpu);
 #define LATENCY_MULTIPLIER			(10000)
 #define SUGOV_KTHREAD_PRIORITY	50
 #define UP_RATE_LIMIT_US (5000)
-#define DOWN_RATE_LIMIT_US (400)
+#define DOWN_RATE_LIMIT_US (4000)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
