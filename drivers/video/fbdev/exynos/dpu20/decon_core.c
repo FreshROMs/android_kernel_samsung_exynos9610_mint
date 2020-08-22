7688290c8b1bba2e08e1720957b33fb2b9522caf
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-22 12:30:55 +0200

    drivers: decon: Disable tracing/logging
    org commit : https://github.com/diepquynh/android_kernel_samsung_universal8895/commit/026047f0a815e9968903c044639b6d5457d02dcf#
    
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -61,13 +61,13 @@
 #include "stui_inf.h"
 #endif
 
-int decon_log_level = 6;
+int decon_log_level = 0;
 module_param(decon_log_level, int, 0644);
-int dpu_bts_log_level = 6;
+int dpu_bts_log_level = 0;
 module_param(dpu_bts_log_level, int, 0644);
-int win_update_log_level = 6;
+int win_update_log_level = 0;
 module_param(win_update_log_level, int, 0644);
-int dpu_mres_log_level = 6;
+int dpu_mres_log_level = 0;
 module_param(dpu_mres_log_level, int, 0644);
 int decon_systrace_enable;
 
