abea6906beab1fe5e20777ad519a406ab8bc9d26
prashantpaddune <padadunep@gmail.com>
2020-08-24 12:10:31 +0530

    fbdev: exynos: decon20: core: remove call to trace_puts.
    Signed-off-by: prashantpaddune <padadunep@gmail.com>

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -107,8 +107,6 @@ void tracing_mark_write(struct decon_device *decon, char id, char *str1, int val
 		decon_err("%s:argument fail\n", __func__);
 		return;
 	}
-	trace_puts(buf);
-
 }
 
 static void decon_dump_using_dpp(struct decon_device *decon)
