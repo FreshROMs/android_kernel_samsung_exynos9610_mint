b837185735ffa793632492d5d78faa7a3fa9388f
Denis Efremov <efremov@linux.com>
2020-09-12 01:28:36 +0300

    security/sdp/dd_kernel_crypto: fix misleading indentation
    Signed-off-by: Denis Efremov <efremov@linux.com>

--- security/sdp/dd_kernel_crypto.c
+++ security/sdp/dd_kernel_crypto.c
@@ -489,5 +489,5 @@ int dd_sec_crypt_bio_pages(struct dd_info *info, struct bio *orig,
     if (rw == DD_ENCRYPT)
         memcpy(&clone->bi_iter, &iter_backup, sizeof(struct bvec_iter));
 
-	return 0;
+    return 0;
 }
