b788445bd9a5208a3707cbb812be6d6916e19508
ThunderStorms21th <pinakastorm@gmail.com>
2021-02-02 09:16:03 +0100

    block: cfq - added some stuff from Pixel 4
    org commit : https://github.com/switch-gott/S20Kernel/commit/5c4872fc48a75555cf1bcba94bc410a0f7b2fe4c
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -38,6 +38,8 @@ static int cfq_rt_idle_only = 1;
 static const u64 cfq_target_latency = (u64)NSEC_PER_SEC * 3/10; /* 300 ms */
 static const int cfq_hist_divisor = 4;
 static int cfq_max_async_dispatch = 4;
+/* Tunes from Pixel 4 */
+extern struct blkcg *blkcg_bg;
 
 /*
  * offset from end of queue service tree for idle class
@@ -1648,6 +1650,16 @@ static void cfq_pd_init(struct blkg_policy_data *pd)
 {
 	struct cfq_group *cfqg = pd_to_cfqg(pd);
 	struct cfq_group_data *cgd = blkcg_to_cfqgd(pd->blkg->blkcg);
+
+/* Tunes from Pixel 4 */
+	if (pd->blkg->blkcg == &blkcg_root) {
+		cgd->weight = 1000;
+		cgd->group_idle = 2000 * NSEC_PER_USEC;
+	} else if (pd->blkg->blkcg == blkcg_bg) {
+		cgd->weight = 200;
+		cgd->group_idle = 0;
+	}
+/* End */
 
 	cfqg->weight = cgd->weight;
 	cfqg->leaf_weight = cgd->leaf_weight;
