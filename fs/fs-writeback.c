2b590e4233f06664527260b20233c766c6e945e4
ThunderStorms21th <pinakastorm@gmail.com>
2021-03-25 14:59:32 +0100

    fs/fs-writeback: fix move_expired_inodes in use
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -1219,11 +1219,11 @@ static void queue_io(struct bdi_writeback *wb, struct wb_writeback_work *work,
 
 	assert_spin_locked(&wb->list_lock);
 	list_splice_init(&wb->b_more_io, &wb->b_io);
-	moved = move_expired_inodes(&wb->b_dirty, &wb->b_io, dirtied_before);
+	moved = move_expired_inodes(&wb->b_dirty, &wb->b_io, 0, dirtied_before);
 	if (!work->for_sync)
 		time_expire_jif = jiffies - dirtytime_expire_interval * HZ;
 	moved += move_expired_inodes(&wb->b_dirty_time, &wb->b_io,
-				     time_expire_jif);
+				     EXPIRE_DIRTY_ATIME, time_expire_jif);
 	if (moved)
 		wb_io_lists_populated(wb);
 	trace_writeback_queue_io(wb, work, dirtied_before, moved);
