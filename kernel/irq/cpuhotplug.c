e3923bc1b9d1896b1698e0e8e7f394e62f5ddbd7
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-10 20:45:32 +0530

    kernel/irq: cpuhotplug: Make loggings dependent on DEBUG_KERNEL
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/irq/cpuhotplug.c
+++ kernel/irq/cpuhotplug.c
@@ -134,8 +134,10 @@ static bool migrate_one_irq(struct irq_desc *desc)
 	 */
 	err = irq_do_set_affinity(d, affinity, false);
 	if (err) {
+#ifdef CONFIG_DEBUG_KERNEL
 		pr_warn_ratelimited("IRQ%u: set affinity failed(%d).\n",
 				    d->irq, err);
+#endif
 		brokeaff = false;
 	}
 
@@ -167,11 +169,12 @@ void irq_migrate_all_off_this_cpu(void)
 		raw_spin_lock(&desc->lock);
 		affinity_broken = migrate_one_irq(desc);
 		raw_spin_unlock(&desc->lock);
-
+#ifdef CONFIG_DEBUG_KERNEL
 		if (affinity_broken) {
 			pr_warn_ratelimited("IRQ %u: no longer affine to CPU%u\n",
 					    irq, smp_processor_id());
 		}
+#endif
 	}
 
 	if (!cpumask_test_cpu(smp_processor_id(), cpu_lp_mask))
