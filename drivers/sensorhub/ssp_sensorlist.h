22c1122e22b3fea4790d131650229a9430468f36
John Vincent <corcegajovic@gmail.com>
2021-04-13 11:01:53 +0800

    drivers/sensorhub: Use pocket_mode_lite for A50S device
    Ref: Source drop

--- drivers/sensorhub/ssp_sensorlist.h
+++ drivers/sensorhub/ssp_sensorlist.h
@@ -42,7 +42,11 @@
 #define SENSOR_INFO_LIGHT_CCT			   {"light_cct_sensor", true, 24, 12}
 #define SENSOR_INFO_AUTO_BIRGHTNESS		     {"auto_brightness", true, 9, 5}
 #define SENSOR_INFO_VDIS_GYRO			   {"vdis_gyro_sensor", true, 6, 6}
+#if IS_ENABLED(CONFIG_SEC_A50S_PROJECT)
+#define SENSOR_INFO_POCKET_MODE_LITE			 {"pocket_mode_lite", true, 5, 5}
+#else
 #define SENSOR_INFO_POCKET_MODE			 {"pocket_mode_sensor", true, 5, 5}
+#endif
 #define SENSOR_INFO_PROXIMITY_CALIBRATION	       {"proximity_calibration", true, 4, 0}
 #define SENSOR_INFO_SENSORHUB			   {"sensorhub_sensor", true, 0, 2}
 #define SENSOR_INFO_PROTOS_MOTION		       {"protos_motion", true, 1, 1}
