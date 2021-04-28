753c957d909c76062cd2ff903f61950525500acb
John Vincent <corcegajovic@gmail.com>
2021-04-28 20:51:00 +0800

    Revert "usb: Modify mass_storage gadget to work with configfs"
    Samsung seems to already have it in source, just as long as we enable CONFIG_USB_ANDROID_SAMSUNG_COMPOSITE.
    
    This reverts commit 7b65357262547113f5728c4ffd60699525054631.

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -3945,8 +3945,6 @@ static void fsg_free_inst(struct usb_function_instance *fi)
 	kfree(opts);
 }
 
-extern struct device *create_function_device(char *name);
-
 static struct usb_function_instance *fsg_alloc_inst(void)
 {
 	struct fsg_opts *opts;
@@ -3997,9 +3995,6 @@ static struct usb_function_instance *fsg_alloc_inst(void)
 	}
 #endif
 	configfs_add_default_group(&opts->lun0.group, &opts->func_inst.group);
-
-	//create dummy device
-	create_function_device("f_mass_storage");
 
 	return &opts->func_inst;
 
