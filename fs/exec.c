f26a31cfb4fcb8f02661f484b61c2357f20d81b0
root <alexfinhart@gmail.com>
2021-05-15 22:31:58 +0800

    fs: exec: don't affine hwcompose and surfaceflinger on the same cpu
    - this causes regressions, and increase in jitters and power consumption.
    
    Signed-off-by: taalojarvi <sreedevan05@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/exec.c
+++ fs/exec.c
@@ -2074,7 +2074,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
-			set_cpus_allowed_ptr(current, cpu_perf_mask);
+			set_cpus_allowed_ptr(current, cpu_lp_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   SURFACEFLINGER_BIN_PREFIX,
 					   strlen(SURFACEFLINGER_BIN_PREFIX)))) {
