488fe1e8ebdbbec58aa4002a3f3d3f27c576de35
John Vincent <corcegajovic@gmail.com>
2021-05-16 15:59:24 +0800

    mm: Enable reaping on SIG_KILL by default
    Testing has shown that this noticeably improves performance. Reaper can help to reclaim the memory in time.

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -51,7 +51,7 @@
 int sysctl_panic_on_oom;
 int sysctl_oom_kill_allocating_task;
 int sysctl_oom_dump_tasks = 1;
-int sysctl_reap_mem_on_sigkill;
+int sysctl_reap_mem_on_sigkill = 1;
 
 DEFINE_MUTEX(oom_lock);
 
