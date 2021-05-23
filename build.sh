4fdb25261ff701183a2d3857f4ef367700a7d031
John Vincent <git@tensevntysevn.cf>
2021-05-24 07:47:28 +0800

    build: Swap check for user builds on packaging
    See 2d84f1775b371a80b755aa27646063063d0fd6a1

--- build.sh
+++ build.sh
@@ -218,14 +218,14 @@ build_zip() {
 	else
 		mv $(pwd)/tools/aik/${DEVICE_BUILD}/image-new.img $(pwd)/tools/package/others/boot.img -f
 
-		if [[ ! -z ${GITHUB_REF##*/} ]]; then
-			echo "fresh.addon.code=io.tns.shadowx.${GITHUB_REF##*/}" >> $(pwd)/tools/package/others/addon.prop
-			echo "fresh.addon.build=${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}" >> $(pwd)/tools/package/others/addon.prop
-			echo "fresh.addon.version=${GITHUB_RUN_NUMBER}" >> $(pwd)/tools/package/others/addon.prop
-		else
+		if [[ ${GITHUB_REF##*/} == "none0-0" ]]; then
 			echo "fresh.addon.code=user.shadowx" >> $(pwd)/tools/package/others/addon.prop
 			echo "fresh.addon.build=user-build" >> $(pwd)/tools/package/others/addon.prop
 			echo "fresh.addon.version=1" >> $(pwd)/tools/package/others/addon.prop
+		else
+			echo "fresh.addon.code=io.tns.shadowx.${GITHUB_REF##*/}" >> $(pwd)/tools/package/others/addon.prop
+			echo "fresh.addon.build=${GITHUB_REF##*/}-${GITHUB_RUN_NUMBER}" >> $(pwd)/tools/package/others/addon.prop
+			echo "fresh.addon.version=${GITHUB_RUN_NUMBER}" >> $(pwd)/tools/package/others/addon.prop
 		fi
 		
 		cd $(pwd)/tools/package/others
