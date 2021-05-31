9cf09c161b416f03490cf391b9c67041951dc482
John Vincent <git@tensevntysevn.cf>
2021-05-31 20:06:02 +0800

    Revert "schedutil: Remove schedtune boost restriction on slack timer"
    This reverts commit 9a8ec226ed28783d48db744a14e27c05cc0d52b8.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -1244,12 +1244,9 @@ static int sugov_need_slack_timer(unsigned int cpu)
 	struct sugov_cpu *sg_cpu = &per_cpu(sugov_cpu, cpu);
 	struct sugov_exynos *sg_exynos = &per_cpu(sugov_exynos, cpu);
 
-// Remove schedtune boost restriction on slack timer
-/*  
 	if (schedtune_cpu_boost(cpu))
 		return 0;
-*/
-	
+
 	if (sg_cpu->util > sg_exynos->min &&
 		get_next_event_time_ms(cpu) > sg_exynos->expired_time)
 		return 1;
