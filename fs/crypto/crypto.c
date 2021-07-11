eb762ed22c51d13fbbddb4bda06a5cc12ba963bc
Hendra Manudinata <aku.hendramanu@gmail.com>
2021-07-12 06:51:26 +0700

    fs: crypto: Fix unused declaration

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -550,9 +550,10 @@ static int __init fscrypt_init(void)
 #endif
 
 	return 0;
+#ifdef CONFIG_FSCRYPT_SDP
 fail_free_info:
 	kmem_cache_destroy(fscrypt_info_cachep);
-
+#endif
 fail_free_ctx:
 	kmem_cache_destroy(fscrypt_ctx_cachep);
 fail_free_queue:
