088ecb1111abf1c0e1331f2e177d58a554331ba1
John Vincent <git@tensevntysevn.cf>
2021-08-13 16:11:12 +0800

    arm64: defconfig: Bringup SEC_A50S project
    Add support for the Samsung Galaxy A50s
    
    Signed-off-by: John Vincent <git@tensevntysevn.cf>

---
+++ Documentation/device-db/a50s.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# =========================================
+#         _____              _      
+#        |  ___| __ ___  ___| |__   
+#        | |_ | '__/ _ \/ __| '_ \  
+#        |  _|| | |  __/\__ \ | | | 
+#        |_|  |_|  \___||___/_| |_| 
+#                              
+# =========================================
+#  
+#  Fresh DeviceDB Information
+#  The Fresh Project
+#  2021 TenSeventy7
+#  
+#  =========================================
+#
+
+DEVICE_KERNEL_BOARD='SRPSG01A005KU'
+DEVICE_KERNEL_BASE=0x10000000
+DEVICE_KERNEL_PAGESIZE=2048
+DEVICE_KERNEL_OFFSET=0x00008000
+DEVICE_RAMDISK_OFFSET=0xf0000000
+DEVICE_SECOND_OFFSET=0xf0000000
+DEVICE_TAGS_OFFSET=0x00000100
+PLATFORM_VERSION="11.0.0"
+PLATFORM_PATCH_LEVEL="2021-06"
+DEVICE_KERNEL_HEADER=1
+DEVICE_DTB_HASHTYPE='sha1'
