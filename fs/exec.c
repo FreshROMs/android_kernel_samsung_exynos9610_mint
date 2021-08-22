beb10fc3e4ea91ba2abcb4eca105fd14e525c1a7
John Vincent <git@tensevntysevn.cf>
2021-08-22 14:45:20 +0800

    kernel: Affine Samsung's HyPer HAL to the big CPU cluster
    While not a critical system service, it pretty much handles boosting on One UI so it might be ideal that this service runs on the faster CPU cluster.
    
    Signed-off-by: John Vincent <git@tensevntysevn.cf>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/exec.c
+++ fs/exec.c
@@ -92,6 +92,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define SEC_WLBTD_BIN_PREFIX "/vendor/bin/wlbtd"
 #define SEC_WLAN_HAL_BIN_PREFIX "/vendor/bin/hw/vendor.samsung.hardware.wifi"
+#define SEM_HYPER_BIN_PREFIX "/vendor/bin/hw/vendor.samsung.hardware.hyper"
 
 void __register_binfmt(struct linux_binfmt * fmt, int insert)
 {
@@ -2057,6 +2058,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 		if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
+			current->flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   SEM_HYPER_BIN_PREFIX,
+					   strlen(SEM_HYPER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name, SEC_WLBTD_BIN_PREFIX, strlen(SEC_WLBTD_BIN_PREFIX))) ||
