f95098d0002ee21ee3430d442a22fccef65452e7
Diep Quynh <remilia.1505@gmail.com>
2021-09-04 23:34:58 +0700

    sched: frt: Initial prefer_perf adaption for Fluid RT
    This will help boosting critical tasks using schedtune and EMS service
    task API
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -165,6 +165,9 @@ static int frt_find_prefer_cpu(struct task_struct *task)
 	struct frt_dom *dom;
 
 	list_for_each_entry(dom, &frt_list, list) {
+		if (schedtune_prefer_perf(task) && is_slowest_cpu(cpumask_first(&dom->cpus)))
+			continue;
+
 		coverage_thr = per_cpu(frt_rqs, cpumask_first(&dom->cpus))->coverage_thr;
 		for_each_cpu_and(cpu, &task->cpus_allowed, &dom->cpus) {
 			allowed_cpu = cpu;
@@ -2717,6 +2720,7 @@ static int find_idle_cpu(struct task_struct *task, int wake_flags)
 	u64 cpu_load, min_load = ULLONG_MAX;
 	struct cpumask candidate_cpus;
 	struct frt_dom *dom, *prefer_dom;
+	bool prefer_perf = schedtune_prefer_perf(task) > 0;
 
 	cpu = frt_find_prefer_cpu(task);
 	prefer_dom = dom = per_cpu(frt_rqs, cpu);
@@ -2732,6 +2736,10 @@ static int find_idle_cpu(struct task_struct *task, int wake_flags)
 		for_each_cpu_and(cpu, &dom->cpus, &candidate_cpus) {
 			if (!idle_cpu(cpu))
 				continue;
+
+			if (prefer_perf && is_slowest_cpu(cpu))
+				continue;
+
 			cpu_prio = cpu_rq(cpu)->rt.highest_prio.curr;
 			if (cpu_prio < max_prio)
 				continue;
@@ -2769,6 +2777,7 @@ static int find_recessive_cpu(struct task_struct *task, int wake_flags)
 	struct cpumask *lowest_mask;
 	struct cpumask candidate_cpus;
 	struct frt_dom *dom, *prefer_dom;
+	bool prefer_perf = schedtune_prefer_perf(task) > 0;
 
 	lowest_mask = this_cpu_cpumask_var_ptr(local_cpu_mask);
 	/* Make sure the mask is initialized first */
@@ -2788,6 +2797,9 @@ static int find_recessive_cpu(struct task_struct *task, int wake_flags)
 
 	do {
 		for_each_cpu_and(cpu, &dom->cpus, &candidate_cpus) {
+			if (prefer_perf && is_slowest_cpu(cpu))
+				continue;
+
 			cpu_load = frt_cpu_util_wake(cpu, task) + task_util(task);
 
 			if (cpu_load > capacity_orig_of(cpu))
@@ -2817,6 +2829,7 @@ static int find_recessive_cpu(struct task_struct *task, int wake_flags)
 
 static int find_lowest_rq_fluid(struct task_struct *task, int wake_flags)
 {
+	bool prefer_perf = schedtune_prefer_perf(task) > 0;
 	int cpu, best_cpu = -1;
 
 	if (task->nr_cpus_allowed == 1) {
@@ -2848,6 +2861,9 @@ static int find_lowest_rq_fluid(struct task_struct *task, int wake_flags)
 	 */
 	for_each_cpu(cpu, cpu_active_mask) {
 		if (cpu != cpumask_first(cpu_coregroup_mask(cpu)))
+			continue;
+
+		if (prefer_perf && is_slowest_cpu(cpu))
 			continue;
 
 		if (find_victim_rt_rq(task, cpu_coregroup_mask(cpu), &best_cpu) != -1)
