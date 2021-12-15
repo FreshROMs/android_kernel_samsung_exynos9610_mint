e49567cbe18ed914fd42fa33cf10898b2359af6f
Cyber Knight <cyberknight755@gmail.com>
2021-12-15 13:22:48 +0800

    fuse: Import `fuse_shortcircuit` from OxygenOS 11.0.0.2 drop
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1939,6 +1939,7 @@ static __latent_entropy struct task_struct *copy_process(
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
 #endif
+	p->fpack = NULL;
 
 	/* Perform scheduler related setup. Assign this task to a CPU. */
 	retval = sched_fork(clone_flags, p);
