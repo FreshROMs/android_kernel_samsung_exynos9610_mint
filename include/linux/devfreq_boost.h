03fd54813d86c9ec3ea673fe695681dc48423fc1
Sultan Alsawaf <sultan@kerneltoast.com>
2022-01-06 09:57:53 +0700

    devfreq/devfreq_boost: Merge 2021 version
    From https://github.com/kerneltoast/android_kernel_google_floral/blob/11.0.0-sultan/drivers/devfreq/devfreq_boost.c
    
    Signed-off-by: repinger <evanf780@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 /*
- * Copyright (C) 2018-2019 Sultan Alsawaf <sultan@kerneltoast.com>.
+ * Copyright (C) 2018-2021 Sultan Alsawaf <sultan@kerneltoast.com>.
  */
 #ifndef _DEVFREQ_BOOST_H_
 #define _DEVFREQ_BOOST_H_
