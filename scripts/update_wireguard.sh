c0994afdc830bff75e1730f9f510fe52222ab4ab
repinger <evanf780@gmail.com>
2022-01-08 17:34:03 +0700

    scripts: update_wireguard: Update
    Signed-off-by: repinger <evanf780@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- scripts/update_wireguard.sh
+++ scripts/update_wireguard.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-git clone git://git.zx2c4.com/wireguard-linux-compat
+git clone --depth=1 git://git.zx2c4.com/wireguard-linux-compat
 rm -fr net/wireguard/*
 
 perl -i -ne 'BEGIN{$print=1;} $print = 0 if m/cat/; print $_ if $print;' wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
@@ -12,4 +12,9 @@ rm -fr wireguard-linux-compat
 git checkout net/wireguard/crypto/zinc/chacha20/.gitignore \
 	     net/wireguard/crypto/zinc/poly1305/.gitignore \
 	     net/wireguard/Kconfig
-	     
+
+git add .
+
+git commit --signoff \
+	   --author="Jason A. Donenfeld <Jason@zx2c4.com>" \
+	   -m "net/wireguard: Sync to git.zx2c4.com/wireguard-linux-compat"
