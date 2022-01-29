00224b6232752276e7f36e60bc06326ec718a7aa
John Vincent <git@tenseventyseven.cf>
2022-01-29 13:47:29 +0800

    fbdev: exynos: Add null-checking to some functions
    The changes can also be seen on Android 12 kernels for Exynos devices, as well as the November security update for the Samsung A50.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -2008,6 +2008,10 @@ static int decon_set_hdr_info(struct decon_device *decon,
 #else
 	video_meta = (struct exynos_video_meta *)dma_buf_vmap(
 			regs->dma_buf_data[win_num][meta_plane].dma_buf);
+	if (IS_ERR_OR_NULL(video_meta)) {
+		decon_err("Failed to get virtual address (err %pK)\n", video_meta);
+		return -ENOMEM;
+	}
 #endif
 
 	hdr_cmp = memcmp(&decon->prev_hdr_info,
@@ -3392,6 +3396,10 @@ static int decon_fb_alloc_memory(struct decon_device *decon, struct decon_win *w
 	}
 
 	vaddr = dma_buf_vmap(buf);
+	if (IS_ERR_OR_NULL(vaddr)) {
+		dev_err(decon->dev, "dma_buf_vmap() failed\n");
+		goto err_map;
+	}
 #endif
 
 	memset(vaddr, 0x00, size);
