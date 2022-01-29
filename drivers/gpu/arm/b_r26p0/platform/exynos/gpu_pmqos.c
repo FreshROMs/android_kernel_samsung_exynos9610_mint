cb16627669aa7941c06bbea4913c4730a0eb7e49
diepquynh <remilia.1505@gmail.com>
2022-01-29 19:35:58 +0800

    gpu: b_r26p0: Reset maximum big cluster frequency when not QoS locked
    If the GPU utilization hasn't reached DVFS lock tipping point, reset
    the big cluster maximum frequency to default
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/arm/b_r26p0/platform/exynos/gpu_pmqos.c
+++ drivers/gpu/arm/b_r26p0/platform/exynos/gpu_pmqos.c
@@ -127,8 +127,14 @@ int gpu_pm_qos_command(struct exynos_context *platform, gpu_pmqos_state state)
 			pm_qos_update_request(&exynos5_g3d_cpu_cluster1_max_qos, platform->table[platform->step].cpu_big_max_freq);
 #if PM_QOS_CPU_CLUSTER_NUM == 3
 		pm_qos_update_request(&exynos5_g3d_cpu_cluster1_min_qos, platform->table[platform->step].cpu_middle_min_freq);
-		if (!platform->boost_is_enabled && platform->env_data.utilization > 45)
-			pm_qos_update_request(&exynos5_g3d_cpu_cluster2_max_qos, platform->table[platform->step].cpu_big_max_freq);
+		if (!platform->boost_is_enabled) {
+			if (platform->env_data.utilization > 45)
+				pm_qos_update_request(&exynos5_g3d_cpu_cluster2_max_qos, 
+					platform->table[platform->step].cpu_big_max_freq);
+			else
+				pm_qos_update_request(&exynos5_g3d_cpu_cluster2_max_qos, 
+						PM_QOS_CLUSTER1_FREQ_MAX_DEFAULT_VALUE);
+		}
 #ifdef CONFIG_MALI_SUSTAINABLE_OPT
 		if (platform->sustainable.info_array[0] > 0) {
 			if (((platform->cur_clock == platform->sustainable.info_array[0])
