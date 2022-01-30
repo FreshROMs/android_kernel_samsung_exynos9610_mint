d84bee5218327cf72bf186f4ceb5719cb2ee4437
John Vincent <git@tenseventyseven.cf>
2022-01-30 12:38:12 +0800

    [FORWARD-PORT] devfreq_boost: Completely remove input boosting
    We're only going to use devfreq_boost for device wakeup and other custom functions on the kernel. Samsung already provides an "input booster" in the kernel, complete with MIF boosting. It's useless, and might be even expensive on overhead, to even have this.
    
    Let's completely remove input boosting on devfreq_boost and rename methods to be just for short MIF boosting, as decon (and TZ) also uses it for boosting.
    
    This is a re-work of an older patch that removes input boosting. Formerly, the bit is named "LIGHT BOOST" but is now standardized as "MIF boost" and is re-worked to adapt with the latest available `devfreq_boost` driver.
    
    Signed-off-by: John Vincent <git@tensevntysevn.cf>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -16,20 +16,20 @@
 static unsigned long devfreq_boost_freq =
 	CONFIG_DEVFREQ_EXYNOS_MIF_BOOST_FREQ;
 static unsigned short devfreq_boost_dur =
-	CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS;
+	CONFIG_DEVFREQ_MIF_BOOST_DURATION_MS;
 
 module_param(devfreq_boost_freq, long, 0644);
 module_param(devfreq_boost_dur, short, 0644);
 
 enum {
 	SCREEN_OFF,
-	INPUT_BOOST,
+	MIF_BOOST,
 	MAX_BOOST
 };
 
 struct boost_dev {
 	struct devfreq *df;
-	struct delayed_work input_unboost;
+	struct delayed_work device_unboost;
 	struct delayed_work max_unboost;
 	wait_queue_head_t boost_waitq;
 	atomic_long_t max_boost_expires;
@@ -42,13 +42,13 @@ struct df_boost_drv {
 	struct notifier_block fb_notif;
 };
 
-static void devfreq_input_unboost(struct work_struct *work);
+static void devfreq_device_unboost(struct work_struct *work);
 static void devfreq_max_unboost(struct work_struct *work);
 
 #define BOOST_DEV_INIT(b, dev, freq) .devices[dev] = {				\
-	.input_unboost =							\
-		__DELAYED_WORK_INITIALIZER((b).devices[dev].input_unboost,	\
-					   devfreq_input_unboost, 0),		\
+	.device_unboost =							\
+		__DELAYED_WORK_INITIALIZER((b).devices[dev].device_unboost,	\
+					   devfreq_device_unboost, 0),		\
 	.max_unboost =								\
 		__DELAYED_WORK_INITIALIZER((b).devices[dev].max_unboost,	\
 					   devfreq_max_unboost, 0),		\
@@ -67,11 +67,11 @@ static void __devfreq_boost_kick(struct boost_dev *b)
 	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
 		return;
 
-	set_bit(INPUT_BOOST, &b->state);
-	if (!mod_delayed_work(system_unbound_wq, &b->input_unboost,
+	set_bit(MIF_BOOST, &b->state);
+	if (!mod_delayed_work(system_unbound_wq, &b->device_unboost,
 		msecs_to_jiffies(devfreq_boost_dur))) {
 		/* Set the bit again in case we raced with the unboost worker */
-		set_bit(INPUT_BOOST, &b->state);
+		set_bit(MIF_BOOST, &b->state);
 		wake_up(&b->boost_waitq);
 	}
 }
@@ -128,12 +128,12 @@ void devfreq_register_boost_device(enum df_device device, struct devfreq *df)
 	WRITE_ONCE(b->df, df);
 }
 
-static void devfreq_input_unboost(struct work_struct *work)
+static void devfreq_device_unboost(struct work_struct *work)
 {
 	struct boost_dev *b = container_of(to_delayed_work(work),
-					   typeof(*b), input_unboost);
+					   typeof(*b), device_unboost);
 
-	clear_bit(INPUT_BOOST, &b->state);
+	clear_bit(MIF_BOOST, &b->state);
 	wake_up(&b->boost_waitq);
 }
 
@@ -156,7 +156,7 @@ static void devfreq_update_boosts(struct boost_dev *b, unsigned long state)
 		df->min_freq = df->profile->freq_table[first_freq_idx];
 		df->max_boost = false;
 	} else {
-		df->min_freq = state & BIT(INPUT_BOOST) ?
+		df->min_freq = state & BIT(MIF_BOOST) ?
 			       min(devfreq_boost_freq, df->max_freq) :
 			       df->profile->freq_table[first_freq_idx];
 		df->max_boost = state & BIT(MAX_BOOST);
@@ -222,90 +222,6 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 	return NOTIFY_OK;
 }
 
-static void devfreq_boost_input_event(struct input_handle *handle,
-				      unsigned int type, unsigned int code,
-				      int value)
-{
-	struct df_boost_drv *d = handle->handler->private;
-	int i;
-
-	for (i = 0; i < DEVFREQ_MAX; i++)
-		__devfreq_boost_kick(&d->devices[i]);
-}
-
-static int devfreq_boost_input_connect(struct input_handler *handler,
-				       struct input_dev *dev,
-				       const struct input_device_id *id)
-{
-	struct input_handle *handle;
-	int ret;
-
-	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
-	if (!handle)
-		return -ENOMEM;
-
-	handle->dev = dev;
-	handle->handler = handler;
-	handle->name = "devfreq_boost_handle";
-
-	ret = input_register_handle(handle);
-	if (ret)
-		goto free_handle;
-
-	ret = input_open_device(handle);
-	if (ret)
-		goto unregister_handle;
-
-	return 0;
-
-unregister_handle:
-	input_unregister_handle(handle);
-free_handle:
-	kfree(handle);
-	return ret;
-}
-
-static void devfreq_boost_input_disconnect(struct input_handle *handle)
-{
-	input_close_device(handle);
-	input_unregister_handle(handle);
-	kfree(handle);
-}
-
-static const struct input_device_id devfreq_boost_ids[] = {
-	/* Multi-touch touchscreen */
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT |
-			INPUT_DEVICE_ID_MATCH_ABSBIT,
-		.evbit = { BIT_MASK(EV_ABS) },
-		.absbit = { [BIT_WORD(ABS_MT_POSITION_X)] =
-			BIT_MASK(ABS_MT_POSITION_X) |
-			BIT_MASK(ABS_MT_POSITION_Y) }
-	},
-	/* Touchpad */
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_KEYBIT |
-			INPUT_DEVICE_ID_MATCH_ABSBIT,
-		.keybit = { [BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH) },
-		.absbit = { [BIT_WORD(ABS_X)] =
-			BIT_MASK(ABS_X) | BIT_MASK(ABS_Y) }
-	},
-	/* Keypad */
-	{
-		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
-		.evbit = { BIT_MASK(EV_KEY) }
-	},
-	{ }
-};
-
-static struct input_handler devfreq_boost_input_handler = {
-	.event		= devfreq_boost_input_event,
-	.connect	= devfreq_boost_input_connect,
-	.disconnect	= devfreq_boost_input_disconnect,
-	.name		= "devfreq_boost_handler",
-	.id_table	= devfreq_boost_ids
-};
-
 static int __init devfreq_boost_init(void)
 {
 	struct df_boost_drv *d = &df_boost_drv_g;
@@ -325,25 +241,16 @@ static int __init devfreq_boost_init(void)
 		}
 	}
 
-	devfreq_boost_input_handler.private = d;
-	ret = input_register_handler(&devfreq_boost_input_handler);
-	if (ret) {
-		pr_err("Failed to register input handler, err: %d\n", ret);
-		goto stop_kthreads;
-	}
-
 	d->fb_notif.notifier_call = fb_notifier_cb;
 	d->fb_notif.priority = INT_MAX;
 	ret = fb_register_client(&d->fb_notif);
 	if (ret) {
 		pr_err("Failed to register fb notifier, err: %d\n", ret);
-		goto unregister_handler;
+		goto stop_kthreads;
 	}
 
 	return 0;
 
-unregister_handler:
-	input_unregister_handler(&devfreq_boost_input_handler);
 stop_kthreads:
 	while (i--)
 		kthread_stop(thread[i]);
