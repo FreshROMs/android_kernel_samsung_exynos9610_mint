ff8caf345f737da0cf9ba86ccd4e14364f7eb108
John Vincent <git@tenseventyseven.cf>
2022-02-04 09:46:23 +0800

    Revert "fuse: Import `fuse_shortcircuit` from OxygenOS 11.0.0.2 drop"
    This reverts commit 929b66082cc1323719f12b7140ac05750470d4aa.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1298,8 +1298,6 @@ struct task_struct {
 	 */
 	randomized_struct_fields_end
 
-	struct fuse_package *fpack;
-
 	/* CPU-specific state of this task: */
 	struct thread_struct		thread;
 
@@ -1309,12 +1307,6 @@ struct task_struct {
 	 *
 	 * Do not put anything below here!
 	 */
-};
-
-struct fuse_package {
-  bool fuse_open_req;
-  struct file *filp;
-  char *iname;
 };
 
 static inline struct pid *task_pid(struct task_struct *task)
