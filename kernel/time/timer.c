9c1aef2784c93b6782ba5884397c74581bf4dc7c
Jebaitedneko <Jebaitedneko@gmail.com>
2022-02-14 20:36:17 +0530

    Revert "Revert "kernel: time: Add delay after cpu_relax() in tight loops""
    This reverts commit a0d9f1d14720916ddf823c99e7be1e1b5f7bd23d.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -927,6 +927,7 @@ static struct timer_base *lock_timer_base(struct timer_list *timer,
 			raw_spin_unlock_irqrestore(&base->lock, *flags);
 		}
 		cpu_relax();
+		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 
@@ -1249,6 +1250,7 @@ int del_timer_sync(struct timer_list *timer)
 		if (ret >= 0)
 			return ret;
 		cpu_relax();
+		ndelay(TIMER_LOCK_TIGHT_LOOP_DELAY_NS);
 	}
 }
 EXPORT_SYMBOL(del_timer_sync);
