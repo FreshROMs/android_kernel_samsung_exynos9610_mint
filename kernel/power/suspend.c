4fa2b3adf251a8dddde90b8a07211ad303180880
aviraxp <wanghan1995315@gmail.com>
2022-02-15 20:28:11 +0530

    power: Introduce OnePlus 3 fingerprintd thaw hack Taken from Oneplus 3, this hack will make fingerprintd recover from suspend quickly.
    Small fixes for newer kernels since we're coming from 3.10.108..
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -86,6 +86,8 @@ void s2idle_set_ops(const struct platform_s2idle_ops *ops)
 	unlock_system_sleep();
 }
 
+extern void thaw_fingerprintd(void);
+
 static void s2idle_begin(void)
 {
 	s2idle_state = S2IDLE_STATE_NONE;
@@ -533,6 +535,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	set_cpus_allowed_ptr(current, &fast_cpu_mask);
 
  Platform_wake:
+	thaw_fingerprintd();
 	platform_resume_noirq(state);
 	dpm_resume_noirq(PMSG_RESUME);
 
