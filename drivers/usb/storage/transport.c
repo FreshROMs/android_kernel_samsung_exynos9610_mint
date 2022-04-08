127d75829d1115ac7e367e492091d5878961790b
John Vincent <git@tenseventyseven.cf>
2022-04-08 20:15:36 +0800

    drivers: usb: Import `usb.h` even if detailed logs are disabled
    usb.h has usb_remove_device() which is still called if device has USB storage detect enabled in defconfig. Causes compilation issues. Fix this.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/usb/storage/transport.c
+++ drivers/usb/storage/transport.c
@@ -63,9 +63,7 @@
 
 #include <linux/blkdev.h>
 #include "../../scsi/sd.h"
-#ifdef CONFIG_USB_DEBUG_DETAILED_LOG
 #include "../core/usb.h"
-#endif
 
 /***********************************************************************
  * Data transfer routines
