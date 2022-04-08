6b671de4f220ebc33aaf9da631d845ccdd4cf115
Sidath Senanayake <sidaths@google.com>
2022-04-08 17:59:46 +0800

    gpu: arm: bv_r32p1: Set mali_jd_thread to RT-prio 60.
    mali_jd_thread is on the wakeup path for graphics and compute jobs
    and should not be delayed.
    
    Bug: 169955425
    Bug: 169348040
    Change-Id: I19caae64a4d65dc8769f2c3d94a05e6d9792940b
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/arm/bv_r32p1/device/backend/mali_kbase_device_jm.c
+++ drivers/gpu/arm/bv_r32p1/device/backend/mali_kbase_device_jm.c
@@ -19,6 +19,8 @@
  *
  */
 
+#include <linux/sched/rt.h>
+#include <uapi/linux/sched/types.h>
 #include <device/mali_kbase_device_internal.h>
 #include <device/mali_kbase_device.h>
 #include <mali_kbase_hwaccess_instr.h>
@@ -243,10 +245,13 @@ void kbase_device_term(struct kbase_device *kbdev)
 	kbase_mem_halt(kbdev);
 }
 
+#define MALI_JD_THREAD_RT_PRIORITY 60
 int kbase_device_init(struct kbase_device *kbdev)
 {
 	int err = 0;
 	unsigned int i = 0;
+	static const struct sched_param param = { .sched_priority =
+					     MALI_JD_THREAD_RT_PRIORITY };
 
 	dev_info(kbdev->dev, "Kernel DDK version %s", MALI_RELEASE_NAME);
 
@@ -270,6 +275,15 @@ int kbase_device_init(struct kbase_device *kbdev)
 		&kbdev->job_done_worker, "mali_jd_thread");
 	if (IS_ERR(kbdev->job_done_worker_thread)) {
 		err = -ENOMEM;
+		return err;
+	}
+
+	if (sched_setscheduler(kbdev->job_done_worker_thread,
+				SCHED_FIFO, &param)) {
+		dev_warn(kbdev->dev, "mali_jd_thread not set to RT prio");
+	} else {
+		dev_info(kbdev->dev, "mali_jd_thread set to RT prio: %i",
+			 MALI_JD_THREAD_RT_PRIORITY);
 	}
 
 	kthread_init_worker(&kbdev->event_worker);
