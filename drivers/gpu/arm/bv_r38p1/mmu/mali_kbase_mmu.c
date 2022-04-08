5cdf48761eb9166903e1d70636f36128067e4471
Sidath Senanayake <sidaths@google.com>
2022-04-08 18:02:55 +0800

    gpu: arm: bv_r38p1: bail out shrinking if the lock is contended
    If the mmu_lock is contended for a long time in shrinking process
    under kswapd context(e.g., lock holder was cpu.share limited
    so it took 100ms to release the lock), kswapd is stuck on the lock.
    It's not good because it causes more memory pressure in the system.
    
    This patch makes the shrinking functino aware of kswapd context
    to bail out if it's the case for kswapd to proceed reclaiming
    with other reclaimable memory.
    
    Bug: 201414237
    Change-Id: Id9e08873def1cf63022937b88af45531d76e2d96
    Signed-off-by: Sidath Senanayake <sidaths@google.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/arm/bv_r38p1/mmu/mali_kbase_mmu.c
+++ drivers/gpu/arm/bv_r38p1/mmu/mali_kbase_mmu.c
@@ -25,6 +25,7 @@
 
 #include <linux/kernel.h>
 #include <linux/dma-mapping.h>
+#include <linux/swap.h>
 #include <mali_kbase.h>
 #include <gpu/mali_kbase_gpu_fault.h>
 #include <gpu/mali_kbase_gpu_regmap.h>
@@ -1874,7 +1875,17 @@ int kbase_mmu_insert_pages_no_flush(struct kbase_device *kbdev, struct kbase_mmu
 	if (nr == 0)
 		return 0;
 
-	mutex_lock(&mmut->mmu_lock);
+	if (!mutex_trylock(&mmut->mmu_lock)) {
+		/*
+		 * Sometimes, mmu_lock takes long time to be released.
+		 * In that case, kswapd is stuck until it can hold
+		 * the lock. Instead, just bail out here so kswapd
+		 * could reclaim other pages.
+		 */
+		if (current_is_kswapd())
+			return -EBUSY;
+		mutex_lock(&mmut->mmu_lock);
+	}
 
 	while (remain) {
 		unsigned int i;
