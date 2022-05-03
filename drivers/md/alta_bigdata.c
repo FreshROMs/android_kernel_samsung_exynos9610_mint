e66a48427c889611eec7aa583aba724f3107ec5c
John Vincent <git@tenseventyseven.cf>
2022-05-03 09:49:57 +0800

    UPSTREAM: md: alta_bigdata: Use spinlocks
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/md/alta_bigdata.c
+++ drivers/md/alta_bigdata.c
@@ -3,12 +3,16 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/highmem.h>
-#include "dm-verity-debug.h"
 #include <linux/kdev_t.h>
 #include<linux/sysfs.h> 
 #include<linux/kobject.h>
 
+#include "dm-verity-debug.h"
+
 #define ALTA_BUF_SIZE    4096
+
+static DEFINE_SPINLOCK(alta_lock);
+
 char* alta_buf;
 size_t * alta_offset,alta_size;
 
@@ -100,6 +104,7 @@ ssize_t alta_bigdata_read(struct file *filep, char __user *buf, size_t size, lof
     if(!proc_buf)
         return -ENOMEM;
 
+    spin_lock(&alta_lock);
     set_print_buf(proc_buf,&proc_offset,ALTA_BUF_SIZE);
 
     /* Print DMV info */
@@ -111,6 +116,7 @@ ssize_t alta_bigdata_read(struct file *filep, char __user *buf, size_t size, lof
         show_dmv_ctr_list();
         show_fc_blks_list();
     }
+    spin_unlock(&alta_lock);
 
     ret = simple_read_from_buffer(buf, size, offset, proc_buf, proc_offset);
     kfree(proc_buf);
