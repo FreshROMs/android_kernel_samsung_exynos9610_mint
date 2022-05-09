89ab1ed7d428ce6081d6c1935eef0c9669f73eee
John Vincent <git@tenseventyseven.cf>
2022-05-09 12:45:12 +0800

    Revert "mm / compaction: Disable compaction of unevictable pages"
    This reverts commit bc2f9da9553cc68a1ba56e7509005e1dd27f2033.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1593,7 +1593,7 @@ typedef enum {
 #ifdef CONFIG_PREEMPT_RT
 int sysctl_compact_unevictable_allowed __read_mostly = 0;
 #else
-int sysctl_compact_unevictable_allowed __read_mostly = 0;
+int sysctl_compact_unevictable_allowed __read_mostly = 1;
 #endif
 
 static inline void
