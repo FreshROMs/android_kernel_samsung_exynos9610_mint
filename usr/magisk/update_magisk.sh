8f1975a67ce12735cb485738ace8cd26cc0254ca
John Vincent <git@tenseventyseven.cf>
2022-05-11 12:35:21 +0800

    update_magisk.sh: Use a different API to fetch and download stable Magisk releases
    Use the one used by the Magisk app itself so we avoid instances where the GitHub page removes the latest release from the Releases page and we get an older version instead of the latest available offered by the app.
    
    Requires `jq` to be installed.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -12,11 +12,11 @@ then
 	magisk_link="https://github.com/topjohnwu/magisk-files/raw/${nver}/app-debug.apk"
 else
 	if [ "x$1" = "x" ]; then
-		nver="$(curl -s https://github.com/topjohnwu/Magisk/releases | grep -m 1 -Poe 'Magisk v[\d\.]+' | cut -d ' ' -f 2)"
+		nver="$(curl -s https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json | jq '.magisk.version' | cut -d '"' -f 2)"
 	else
 		nver="$1"
 	fi
-	magisk_link="https://github.com/topjohnwu/Magisk/releases/download/${nver}/Magisk-${nver}.apk"
+	magisk_link="https://cdn.jsdelivr.net/gh/topjohnwu/magisk-files@${nver}/app-release.apk"
 fi
 
 if [ \( -n "$nver" \) -a \( "$nver" != "$ver" \) -o ! \( -f "$DIR/arm/magiskinit64" \) -o \( "$nver" = "canary" \) ]
