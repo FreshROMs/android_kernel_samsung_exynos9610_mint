b5cdde344335968866a7e8a1fc25b3fefa5d351e
John Vincent <git@tenseventyseven.cf>
2022-05-12 13:12:24 +0800

    kernel: configs: Separate ARM Mali configs based on Android version
    Android 11 uses R26P0, while Android 12 uses R32P1.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- build.sh
+++ build.sh
@@ -557,6 +557,7 @@ get_devicedb_info
 
 # All variants get semi-Knox while we're fixing compile issues
 merge_config partial-deknox-${BUILD_ANDROID_PLATFORM}
+merge_config mali-${BUILD_ANDROID_PLATFORM}
 merge_config variant_${BUILD_KERNEL_CODE}
 
 if [[ ${BUILD_KERNEL_PERMISSIVE} == 'true' ]]; then
