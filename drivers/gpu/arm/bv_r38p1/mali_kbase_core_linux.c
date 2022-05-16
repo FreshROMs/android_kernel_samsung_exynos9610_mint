f8290dd7d1a42413b200c47ae2fc79b2cc4fb7da
John Vincent <git@tenseventyseven.cf>
2022-05-16 13:18:19 +0800

    Revert "gpu: arm: bv_r38p1: sync wakeup on event handoff from job done."
    Subsequent merges added additional callsites for this wakeup that might not make sense. Reverting this seems to avert a long-runnable problem, so revert while targetting irq wakeup path more directly.
    
    This reverts commit ec11d6f6f78ad2e6618bc9286d1400a03c883a42.
    
    Bug: 187129999
    Co-authored-by: kdub <kdub@users.noreply.github.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/gpu/arm/bv_r38p1/mali_kbase_core_linux.c
+++ drivers/gpu/arm/bv_r38p1/mali_kbase_core_linux.c
@@ -2262,7 +2262,7 @@ void kbase_event_wakeup(struct kbase_context *kctx)
 	KBASE_DEBUG_ASSERT(kctx);
 	dev_dbg(kctx->kbdev->dev, "Waking event queue for context %pK\n",
 		(void *)kctx);
-	wake_up_interruptible_sync(&kctx->event_queue);
+	wake_up_interruptible(&kctx->event_queue);
 }
 
 KBASE_EXPORT_TEST_API(kbase_event_wakeup);
