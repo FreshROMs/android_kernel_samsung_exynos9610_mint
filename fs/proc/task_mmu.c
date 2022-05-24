49fbdd767a0ac026bf3acf89e80b7fd096aeb068
John Vincent <git@tenseventyseven.cf>
2022-05-24 10:49:59 +0800

    proc: task_mmu: Defer page reclaim and writeback when PM is frozen
    A little tweak done by Samsung on newer kernels possibly to save battery life as this will defer any page reclaim and writeback when processes are frozen. This is also possibly a way to prevent a device lockup.
    
    There are multiple ways to do this, but what Samsung did for the Galaxy S10 seems to be the easiest to implement as this doesn't need additional functions ('pm_freezing' is a publicly-accessible variable anyway).
    
    Pulled from the Galaxy S10 Android 12 sources.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -20,6 +20,7 @@
 #include <linux/uaccess.h>
 #include <linux/mm_inline.h>
 #include <linux/ctype.h>
+#include <linux/freezer.h>
 
 #include <asm/elf.h>
 #include <asm/tlb.h>
@@ -1847,6 +1848,8 @@ static int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
 cont:
 	if (rwsem_is_contended(&walk->mm->mmap_sem))
 		return -1;
+	if (pm_freezing)
+		return -1;
 
 	isolated = 0;
 	pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
@@ -1914,6 +1917,8 @@ static int writeback_pte_range(pmd_t *pmd, unsigned long addr,
 	if (pmd_trans_unstable(pmd))
 		return 0;
 	if (rwsem_is_contended(&mm->mmap_sem))
+		return -1;
+	if (pm_freezing)
 		return -1;
 	if (zram_is_app_launch())
 		return -EBUSY;
