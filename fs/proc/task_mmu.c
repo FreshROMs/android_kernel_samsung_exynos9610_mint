d85043623009d9346dd97c44be9e7538f2a182e7
John Vincent <git@tenseventyseven.cf>
2022-05-24 08:54:00 +0800

    proc: task_mmu: Improvements to per-process writeback
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1857,16 +1857,19 @@ static int reclaim_pte_range(pmd_t *pmd, unsigned long addr,
 	LIST_HEAD(page_list);
 	int isolated;
 
+#ifdef CONFIG_ZRAM_LRU_WRITEBACK
+	bool is_lru_wb = false;
+
+	if (!strcmp("PerProcessNands", current->comm))
+		is_lru_wb = true;
+#endif
+
 	split_huge_pmd(vma, pmd, addr);
 	if (pmd_trans_unstable(pmd))
 		return 0;
 cont:
 	if (rwsem_is_contended(&walk->mm->mmap_sem))
 		return -1;
-#ifdef CONFIG_ZRAM_LRU_WRITEBACK
-	if (zram_is_app_launch())
-		return -1;
-#endif
 
 	isolated = 0;
 	pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
@@ -1882,13 +1885,13 @@ cont:
 		if (PageUnevictable(page))
 			continue;
 
-#ifdef CONFIG_ZRAM_LRU_WRITEBACK
-		if (ptep_test_and_clear_young(vma, addr, pte))
-			continue;
-#endif
-
 		if (!PageLRU(page))
 			continue;
+
+#ifdef CONFIG_ZRAM_LRU_WRITEBACK
+		if (is_lru_wb && ptep_test_and_clear_young(vma, addr, pte))
+			continue;
+#endif
 
 		if (isolate_lru_page(compound_head(page)))
 			continue;
@@ -1931,12 +1934,12 @@ static int writeback_pte_range(pmd_t *pmd, unsigned long addr,
 	spinlock_t *ptl;
 	LIST_HEAD(swp_entry_list);
 
-	if (pmd_trans_huge(*pmd))
+	if (pmd_trans_unstable(pmd))
 		return 0;
 	if (rwsem_is_contended(&mm->mmap_sem))
 		return -1;
 	if (zram_is_app_launch())
-		return -1;
+		return -EBUSY;
 
 	pte = pte_offset_map_lock(mm, pmd, addr, &ptl);
 	for (; addr != end; pte++, addr += PAGE_SIZE) {
@@ -2048,8 +2051,6 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 			return -ENOMEM;
 		}
 	}
-	while (zram_is_app_launch())
-		msleep(1000);
 #endif
 
 	mm = get_task_mm(task);
