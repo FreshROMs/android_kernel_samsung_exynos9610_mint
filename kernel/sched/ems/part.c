ad43387d8fab30a50e686c1bcc8dc92124d5b895
John Vincent <git@tenseventyseven.cf>
2022-07-25 11:11:18 +0800

    sched: ems: part: Align boost interval and period size to kernel's PELT halflife
    Mint uses 12ms of PELT half-life. Align PART's period size and boost period to this to ensure it captures utilization at least twice, and boosts at least once during a scheduler tick.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/ems/part.c
+++ kernel/sched/ems/part.c
@@ -38,7 +38,7 @@ char *part_policy_name[] = {
 };
 
 static __read_mostly unsigned int part_policy_idx = PART_POLICY_MAX_RECENT_LAST;
-static __read_mostly u64 period_size = 8 * NSEC_PER_MSEC;
+static __read_mostly u64 period_size = 6 * NSEC_PER_MSEC;
 static __read_mostly u64 period_hist_size = 10;
 static __read_mostly int high_patten_thres = 700;
 static __read_mostly int high_patten_stdev = 200;
@@ -46,7 +46,7 @@ static __read_mostly int low_patten_count = 3;
 static __read_mostly int low_patten_thres = 1024;
 static __read_mostly int low_patten_stdev = 200;
 
-static __read_mostly u64 boost_interval = 16 * NSEC_PER_MSEC;
+static __read_mostly u64 boost_interval = 12 * NSEC_PER_MSEC;
 
 /********************************************************/
 /*		  Helper funcition			*/
