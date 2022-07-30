6a1d193dfa249c5897b672dec0c824b80af06316
John Vincent <git@tenseventyseven.cf>
2022-07-30 09:55:36 +0800

    fbdev: exynos: decon: Run vsync thread as RT
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/video/fbdev/exynos/dpu20/decon_dsi.c
+++ drivers/video/fbdev/exynos/dpu20/decon_dsi.c
@@ -342,6 +342,7 @@ static int decon_vsync_thread(void *data)
 
 int decon_create_vsync_thread(struct decon_device *decon)
 {
+	struct sched_param param = { .sched_priority = 20 };
 	int ret = 0;
 	char name[16];
 
@@ -366,6 +367,7 @@ int decon_create_vsync_thread(struct decon_device *decon)
 		goto err;
 	}
 
+	sched_setscheduler_nocheck(decon->vsync.thread, SCHED_FIFO, &param);
 	return 0;
 
 err:
