22daac73d1f2d6075ba495464971a599acce7b68
John Vincent <git@tenseventyseven.cf>
2022-11-02 13:18:18 +0800

    sched: ems: Improve 'fast' scheduling path checking
    Put the 'CPU active' check as the very first so we avoid scheduling tasks onto inactive CPUs as much as possible. Also separate the active check and the capacity check just for the compiler to optimize this properly.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/ems/core.c
+++ kernel/sched/ems/core.c
@@ -351,13 +351,13 @@ int exynos_wakeup_balance(struct task_struct *p, int prev_cpu, int sd_flag, int 
 	 * slow path if not needed.
 	 *
 	 */
-	if (cpumask_test_cpu(prev_cpu, tsk_cpus_allowed(p)) && cpu_active(prev_cpu) &&
-		idle_cpu(prev_cpu) && (eenv.start_cpu_cap == get_cpu_max_capacity(prev_cpu)) &&
-		!lbt_util_overutilized(prev_cpu)) {
-		if (idle_get_state_idx(cpu_rq(prev_cpu)) <= 1) {
-			target_cpu = prev_cpu;
-			strcpy(state, "fast path");
-			goto out;
+	if (cpu_active(prev_cpu) && idle_cpu(prev_cpu) && cpumask_test_cpu(prev_cpu, tsk_cpus_allowed(p))) {
+		if ((eenv.start_cpu_cap == get_cpu_max_capacity(prev_cpu)) && !lbt_util_overutilized(prev_cpu)) {
+			if (idle_get_state_idx(cpu_rq(prev_cpu)) <= 1) {
+				target_cpu = prev_cpu;
+				strcpy(state, "fast path");
+				goto out;
+			}
 		}
 	}
 
