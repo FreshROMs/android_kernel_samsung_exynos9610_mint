c5136160b827d62941473fae33459228f3409d85
John Vincent <git@tenseventyseven.cf>
2022-11-22 15:43:15 +0800

    melfas: melfas_mss100: Only probe asynchronously on One UI builds
    Double-tap to wake (DT2W) takes a hit on AOSP kernels, possibly because of the TSP being less actively-probed on non-SESL ROMs. Only probe asynchronously when building One UI variants of the kernel.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
+++ drivers/input/touchscreen/melfas/mss100/melfas_mss100.c
@@ -1899,7 +1899,9 @@ static struct i2c_driver mms_driver = {
 #ifdef CONFIG_PM
 		.pm = &mms_dev_pm_ops,
 #endif
+#ifdef CONFIG_MINT_SESL
 		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
+#endif
 	},
 };
 
