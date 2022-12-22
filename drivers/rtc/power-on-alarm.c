a96558e68854bcddacaefee47e85e50a43d3e035
John Vincent <git@tenseventyseven.cf>
2022-12-22 18:56:47 +0800

    drivers: rtc: Support new alarm boot 'set' command
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/rtc/power-on-alarm.c
+++ drivers/rtc/power-on-alarm.c
@@ -26,12 +26,17 @@
 #include <linux/alarmtimer.h>
 #include <linux/ioctl.h>
 
+struct alarm_timespec {
+	char alarm[14];
+};
+
 #define ANDROID_ALARM_BASE_CMD(cmd)         (cmd & ~(_IOC(0, 0, 0xf0, 0)))
 #define ANDROID_ALARM_SET_ALARM_BOOT	    _IOW('a', 7, struct timespec)
 #ifdef CONFIG_COMPAT
 #define ANDROID_ALARM_SET_ALARM_BOOT_COMPAT	_IOW('a', 7, \
 							struct compat_timespec)
 #endif
+#define ANDROID_ALARM_SET_ALARM_BOOT_NEW    _IOW('a', 7, struct alarm_timespec)
 
 static long power_on_alarm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -40,6 +45,7 @@ static long power_on_alarm_ioctl(struct file *file, unsigned int cmd, unsigned l
 
 	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
 	case ANDROID_ALARM_SET_ALARM_BOOT:
+	case ANDROID_ALARM_SET_ALARM_BOOT_NEW:
 		if (copy_from_user(bootalarm_data, (void __user *)arg, 14)) {
 			rv = -EFAULT;
 			printk("boot alarm param copy fail\n");
@@ -61,6 +67,7 @@ static long power_alarm_compat_ioctl(struct file *file, unsigned int cmd, unsign
 
 	switch (ANDROID_ALARM_BASE_CMD(cmd)) {
 	case ANDROID_ALARM_SET_ALARM_BOOT_COMPAT:
+	case ANDROID_ALARM_SET_ALARM_BOOT_NEW:
 		if (copy_from_user(bootalarm_data, (void __user *)arg, 14)) {
 			rv = -EFAULT;
 			printk("boot alarm param copy fail\n");
