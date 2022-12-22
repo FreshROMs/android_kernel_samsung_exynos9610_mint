624de5f86daf1f7c11a44bc39722e97353de00dd
John Vincent <git@tenseventyseven.cf>
2022-12-22 19:06:25 +0800

    mm: zsmalloc: Trigger zs_compact when an userspace app exits
    Trigger compaction when an Android app exits. Use the new API from 'kernel/fork'. Pulled from the Galaxy A51 sources from December 2022.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- include/linux/zsmalloc.h
+++ include/linux/zsmalloc.h
@@ -57,4 +57,7 @@ unsigned long zs_get_total_pages(struct zs_pool *pool);
 unsigned long zs_compact(struct zs_pool *pool);
 
 void zs_pool_stats(struct zs_pool *pool, struct zs_pool_stats *stats);
+
+void try_schedule_zs_compact(void);
+extern void register_on_app_mmput_callback(void (*callback)(void));
 #endif
