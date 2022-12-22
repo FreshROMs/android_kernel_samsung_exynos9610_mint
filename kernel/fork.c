a036de2b6dc8c7b802f5679968ee6cde7441eda6
John Vincent <git@tenseventyseven.cf>
2022-12-22 19:05:34 +0800

    kernel: Add an API to check if an application exits
    From Samsung. This may be used by modules to, for example, trigger compaction when an app exits. Check if the task is spawned from 'zygote(64)', these means that they're userspace apps.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/fork.c
+++ kernel/fork.c
@@ -971,14 +971,69 @@ static inline void __mmput(struct mm_struct *mm)
 }
 
 /*
+ * Store pids of zygote and zygote64.
+ * Both processes uses "main" as its comm.
+ */
+static pid_t zygote_pids[2];
+
+bool is_app(struct task_struct *p)
+{
+	struct task_struct *parent;
+
+	parent = rcu_dereference(p->real_parent);
+
+	if (parent->pid == zygote_pids[0])
+		return true;
+
+	if (parent->pid == zygote_pids[1])
+		return true;
+
+	if (zygote_pids[0] && zygote_pids[1])
+		return false;
+
+	if (strncmp(parent->comm, "main", 4) == 0) {
+		if (!zygote_pids[0]) {
+			zygote_pids[0] = parent->pid;
+			pr_info("set zygote_pids[0]=%d", parent->pid);
+			return true;
+		}
+		if (!zygote_pids[1]) {
+			zygote_pids[1] = parent->pid;
+			pr_info("set zygote_pids[1]=%d", parent->pid);
+			return true;
+		}
+	}
+	return false;
+}
+
+void (*on_app_mmput_callback)(void);
+
+void call_on_app_mmput_callback(void)
+{
+	if (on_app_mmput_callback) {
+		on_app_mmput_callback();
+	}
+}
+
+void register_on_app_mmput_callback(void (*callback)(void))
+{
+	on_app_mmput_callback = callback;
+}
+EXPORT_SYMBOL_GPL(register_on_app_mmput_callback);
+
+/*
  * Decrement the use count and release all resources for an mm.
  */
 void mmput(struct mm_struct *mm)
 {
 	might_sleep();
 
-	if (atomic_dec_and_test(&mm->mm_users))
+	if (atomic_dec_and_test(&mm->mm_users)) {
 		__mmput(mm);
+		if (is_app(current)) {
+			call_on_app_mmput_callback();
+		}
+	}
 }
 EXPORT_SYMBOL_GPL(mmput);
 
