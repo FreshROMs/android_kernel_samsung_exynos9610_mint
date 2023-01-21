c88893bd6c78e44d2d0522196d20c8b591ee0ba2
John Vincent <git@tenseventyseven.cf>
2023-01-21 14:10:56 +0800

    sched: core: Set UCLAMP_MIN of RT tasks to zero
    The Exynos 1280 (S5E8825) sets 'sched_util_clamp_min_rt_default' to 0 vendor-side.
    
    /vendor/etc/init/init.s5e8825.rc:
    ```
        # Clear sched_util_clamp_min_rt_default
        write /proc/sys/kernel/sched_util_clamp_min_rt_default 0
    ```
    
    Do the same for us, kernel-side, as well.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -791,7 +791,7 @@ unsigned int sysctl_sched_uclamp_util_max = SCHED_CAPACITY_SCALE;
  * This knob will not override the system default sched_util_clamp_min defined
  * above.
  */
-unsigned int sysctl_sched_uclamp_util_min_rt_default = 96;
+unsigned int sysctl_sched_uclamp_util_min_rt_default = 0;
 
 /* All clamps are required to be less or equal than these values */
 static struct uclamp_se uclamp_default[UCLAMP_CNT];
