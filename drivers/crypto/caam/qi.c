13a732c073c2c8ef8c9dea41e2d4cf4578b20a47
John Vincent <git@tenseventyseven.cf>
2023-01-23 13:27:06 +0800

    crypto: caam/qi: Switch to cpus_mask for old cpumask
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- drivers/crypto/caam/qi.c
+++ drivers/crypto/caam/qi.c
@@ -497,7 +497,7 @@ int caam_qi_shutdown(struct device *qidev)
 	int i, ret;
 	struct caam_qi_priv *priv = dev_get_drvdata(qidev);
 	const cpumask_t *cpus = qman_affine_cpus();
-	struct cpumask old_cpumask = current->cpus_allowed;
+	struct cpumask old_cpumask = current->cpus_mask;
 
 	for_each_cpu(i, cpus) {
 		struct napi_struct *irqtask;
@@ -712,7 +712,7 @@ int caam_qi_init(struct platform_device *caam_pdev)
 	struct device *ctrldev = &caam_pdev->dev, *qidev;
 	struct caam_drv_private *ctrlpriv;
 	const cpumask_t *cpus = qman_affine_cpus();
-	struct cpumask old_cpumask = current->cpus_allowed;
+	struct cpumask old_cpumask = current->cpus_mask;
 	static struct platform_device_info qi_pdev_info = {
 		.name = "caam_qi",
 		.id = PLATFORM_DEVID_NONE
